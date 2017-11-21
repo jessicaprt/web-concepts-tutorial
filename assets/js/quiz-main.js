3881d6f06c00b42d898bebce1d791fd776951993
Jessica Prieto <jessicaprieto31@gmail.com>
2017-11-21 16:49:39 -0700

    added a little bit of style and refactored to make code usable by other units

---
+++ assets/js/quiz-main.js
@@ -0,0 +1,69 @@
+function checkQuiz() {
+    console.log("checking quiz");
+    for (var i=0; i < questions.length; i++) {
+        
+//        console.log("qlength: " + questions.length);
+        console.log("checking: q-" + i);
+        var radioOptions = document.getElementsByName("q-" + (i));
+        var chosenButton = getOption(radioOptions);
+        
+        if (chosenButton != -1) {
+            if (questions[i].choices[chosenButton][1] != true) {
+                allWrongAns.push([questions[i], chosenButton]);
+                console.log("Question " + (i+1) + " is wrong");
+            } else {
+                console.log("Question " + (i+1) + " is right");
+            }
+
+        } else {
+            console.log("cannot read radio button: " + i);
+            alert("please complete quiz!");
+            return;
+        } 
+    }
+    var quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = "";
+
+    var results = document.getElementById("result");
+    results.innerHTML = "";
+
+    results.innerHTML += '<a href="unit1-quiz.html" class="quiz-button">Take quiz again!</a><br>';
+
+    results.innerHTML += '<p class="start-result">Result: ' + (questions.length - allWrongAns.length) 
+                          + '/' + questions.length 
+                          + '</p>';
+    showResults(results);
+}
+
+function getOption(radioOptions) {
+    console.log("getting options");
+    var numOptions = radioOptions.length;
+    for (var i=0; i < numOptions; i++) {
+        if(radioOptions[i].checked) {
+           return i;
+        } 
+    }
+    return -1;
+}
+
+function showResults(resultsElement) {
+    if (allWrongAns.length == 0) {
+        resultsElement.innerHTML += '<br>';
+        resultsElement.innerHTML += '<p>Perfect Score!<p>';
+        console.log("wrong length: " + allWrongAns.length);
+    } else {
+        for (var i=0; i<allWrongAns.length; i++) {
+            var currentQuestion = allWrongAns[i][0];
+            var currentChoiceIdx = allWrongAns[i][1];
+            
+            resultsElement.innerHTML += '<br>';
+            resultsElement.innerHTML += currentQuestion.question;
+            resultsElement.innerHTML += '<p><strong>Your answer</strong> : ' + 
+                                        currentQuestion.choices[currentChoiceIdx][0] + 
+                                        '</p>';
+            resultsElement.innerHTML += '<p><strong>Correct answer</strong> : ' +
+                                        currentQuestion.choices[currentQuestion.rightAnsIdx][0] +
+                                        '</p>';
+        }
+    }   
+}
--- assets/js/unit1-quiz.js
+++ assets/js/unit1-quiz.js
@@ -7,49 +7,77 @@ var allWrongAns = [];
 
 for (var i=0; i<numQuestions; i++) {
     questionElements[i] = document.getElementById("question-" + (i+1));
-//    console.log("question-" + i);
-//    console.log(questionElements);
 }
 
 function generateQuiz() {
-    /** QUESTION 1 **/
-    var question1 = "Question 1: To display a numbered or any ordered list on a web page, which HTML element tag is used?";
-    var choices1 = [
-        ["&lt;ul&gt;", false],
-        ["&lt;nl&gt;", false],
-        ["&lt;ol&gt;", true],
-        ["&lt;il&gt;", false]        
-    ];
-    
-    /** QUESTION 2 **/ 
-    var question2 = "Question2: (True or False) for conflicting styles on stylesheets, the latter implementation is ignored. It always uses the first implementation";
-    var choices2 = [
-        ["True", false],
-        ["False", true] 
-    ];
-    
-    /** QUESTION 3 **/
-    var question3 = "Question3: What attribute follows the element 'a' to specify and embedded link?<br><em>&lta .. &gt;</em>";
-    var choices3 = [
-        ["src", false],
-        ["embed", false], 
-        ["link", false],
-        ["href", true] 
-    ];
-    
-    /** QUESTION 4 **/
-    var question4 = "Question4: (True or False) There are three ways to associate a stylesheet in an HTML file: inline, embedded and linking external stylesheets";
-    var choices4 = [
-        ["True", true],
-        ["False", false]
-    ];
-    
     questions = [
-        new quiz(question1, choices1, 2),
-        new quiz(question2, choices2, 1),
-        new quiz(question3, choices3, 3),
-        new quiz(question4, choices4, 0)
-    ];
+        
+        /** QUESTION 1 **/
+        new quiz(
+            "To display a numbered or any ordered list on a web page, which HTML element tag is used?",
+            [
+                ["&lt;ul&gt;", false],
+                ["&lt;nl&gt;", false],
+                ["&lt;ol&gt;", true],
+                ["&lt;il&gt;", false]        
+            ], 2),
+        
+        /** QUESTION 2 **/
+        new quiz(
+            "(True or False) for conflicting styles on stylesheets, the latter implementation is ignored. It always uses the first implementation", 
+            [
+                ["True", false],
+                ["False", true] 
+            ], 1),
+        
+        /** QUESTION 3 **/
+        new quiz(
+            "What attribute follows the element 'a' to specify and embedded link?<br><em>&lta .. &gt;</em>",
+            [
+                ["src", false],
+                ["embed", false], 
+                ["link", false],
+                ["href", true] 
+            ], 3),
+        
+        /** QUESTION 4 **/
+        new quiz(
+            "(True or False) There are three ways to associate a stylesheet in an HTML file: inline, embedded and linking external stylesheets", 
+            [
+                ["True", true],
+                ["False", false]
+            ], 0),
+        
+        /** QUESTION 5 **/
+        new quiz(
+            "Which of the following elements are NOT needed to create a table?",
+            [
+                ["tc", true],
+                ["tr", false],
+                ["th", false],
+                ["td", false],
+            ], 0),
+        
+        /** QUESTION 6 **/
+        new quiz(
+            "CSS stands for:",
+            [
+                ["Creative Style Sheets", false],
+                ["Creative Style Scripts", false],
+                ["Cascading Style Sheets", true],
+                ["Cascading Style Scripts", false]
+            ], 2),
+        
+        /** QUESTION 1 **/
+        new quiz(
+            "Which major HTML element defines some of the properties of the webpage including the title, meta information as well as stylesheets and scripts?",
+            [
+                ["&lt;html&gt;", false],
+                ["&lt;head&gt;", true],
+                ["&lt;body&gt;", false],
+                ["&lt;info&gt;", false],
+            ], 1)        
+        ];
     
     for (var i=0; i<questions.length; i++) {
         questionElements[i].innerHTML = questions[i].question;
@@ -61,76 +89,6 @@ function generateQuiz() {
             currRadioButton.innerHTML += '</input><br>';
         }
     }
-}
-
-function checkQuiz() {
-    console.log("checking quiz");
-    for (var i=0; i < questions.length; i++) {
-        
-//        console.log("qlength: " + questions.length);
-        console.log("checking: q-" + i);
-        var radioOptions = document.getElementsByName("q-" + (i));
-        var chosenButton = getOption(radioOptions);
-        
-        if (chosenButton != -1) {
-            if (questions[i].choices[chosenButton][1] != true) {
-                allWrongAns.push([questions[i], chosenButton]);
-                console.log("Question " + (i+1) + " is wrong");
-            } else {
-                console.log("Question " + (i+1) + " is right");
-            }
-
-        } else {
-            console.log("cannot read radio button: " + i);
-            alert("please complete quiz!");
-            return;
-        } 
-    }
-    var quizElement = document.getElementById("quiz");
-    quizElement.innerHTML = "";
-
-    var results = document.getElementById("result");
-    results.innerHTML = "";
-
-    results.innerHTML += '<a href="unit1-quiz.html"> Start Another Quiz</a><br>';
-
-    results.innerHTML += '<p>Results: ' + (questions.length - allWrongAns.length) 
-                          + '/' + questions.length 
-                          + '</p>';
-    showResults(results);
-}
-
-function getOption(radioOptions) {
-    console.log("getting options");
-    var numOptions = radioOptions.length;
-    for (var i=0; i < numOptions; i++) {
-        if(radioOptions[i].checked) {
-           return i;
-        } 
-    }
-    return -1;
-}
-
-function showResults(resultsElement) {
-    if (allWrongAns.length == 0) {
-        resultsElement.innerHTML += '<br>';
-        resultsElement.innerHTML += '<p>Perfect Score!<p>';
-        console.log("wrong length: " + allWrongAns.length);
-    } else {
-        for (var i=0; i<allWrongAns.length; i++) {
-            var currentQuestion = allWrongAns[i][0];
-            var currentChoiceIdx = allWrongAns[i][1];
-            
-            resultsElement.innerHTML += '<br>';
-            resultsElement.innerHTML += currentQuestion.question;
-            resultsElement.innerHTML += '<p><strong>Your answer</strong> : ' + 
-                                        currentQuestion.choices[currentChoiceIdx][0] + 
-                                        '</p>';
-            resultsElement.innerHTML += '<p><strong>Correct answer</strong> : ' +
-                                        currentQuestion.choices[currentQuestion.rightAnsIdx][0] +
-                                        '</p>';
-        }
-    }   
 }
 
 generateQuiz();
